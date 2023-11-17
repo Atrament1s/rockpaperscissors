35dd97ec617265b76397a83d8203b49fca160fe0
Atrament1s <93157646+Atrament1s@users.noreply.github.com>
2023-11-17 10:39:51 -0600

    bonus

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,4 +27,26 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("Type 0 for rock, 1 for paper, or 2 for scissors\n"))
+
+if user_choice >= 3 or user_choice < 0:
+    print("Invalid number, please input 0, 1, or 2 for rock, paper, or scissors, respectively.")
+else:
+    print("You Chose:")
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer Chose:")
+    print(game_images[computer_choice])
+    if user_choice == 0 and computer_choice == 2:
+        print('You win!')
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose!")
+    elif computer_choice > user_choice:
+        print("You lose!")
+    elif computer_choice < user_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a tie!")
